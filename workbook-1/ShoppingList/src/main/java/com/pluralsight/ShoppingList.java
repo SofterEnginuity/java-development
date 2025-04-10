75353492f8dfe4e60c85765aca0d0f066e12587e
Marqae Morris <mamorris920@gmail.com>
2025-04-10 09:27:43 -0400

    Initial commit

---
+++ workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("Eggs");
+        System.out.println("bagel thins");
+        System.out.println("frozen fruit");
+        System.out.println("pasta");
+        System.out.println("chicken");
+        System.out.println("hangers");
+        System.out.println("light sour cream");
+        System.out.println("broccoli");
+        System.out.println("baby carrots");
+        System.out.println("ginger paste");
+    }
+}
