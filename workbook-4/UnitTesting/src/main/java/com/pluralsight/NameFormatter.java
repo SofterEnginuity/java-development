f66d47400b4c23e70aa23375b790e03fd700bf57
Marqae Morris <mamorris920@gmail.com>
2025-05-08 10:34:03 -0400

    practice creating classes using only static methods

---
+++ workbook-4/UnitTesting/src/main/java/com/pluralsight/NameFormatter.java
@@ -0,0 +1,30 @@
+package com.pluralsight;
+
+public class NameFormatter {
+
+
+    private NameFormatter(String prefix, String firstName, String middleName, String lastName, String suffix) {
+//            this.prefix = prefix;
+//            this.firstName = firstName;
+//            this.middleName = middleName;
+//            this.lastName = lastName;
+//            this.suffix = suffix;
+
+
+    }
+
+    public static String format(String firstName, String lastName) {
+        System.out.println();
+    }
+
+
+    public static String format(String prefix, String firstName, String middleName, String lastName, String suffix) {
+
+
+    }
+
+            public static String format(String fullName){
+
+        }
+
+}
---
+++ workbook-4/UnitTesting/src/main/java/com/pluralsight/StaticClasses.java
@@ -0,0 +1,26 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class StaticClasses {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Please enter your full name");
+        String firstName =  myScanner.nextLine();
+
+
+        }
+
+
+public static String format(String prefix, String firstName, String middleName, String lastName, String suffix){
+
+}
+
+//        public static String format(String firstName, String lastName){
+//            System.out.println();
+//        }
+
+
+//        public static format(String fullName){
+//
+//        }
