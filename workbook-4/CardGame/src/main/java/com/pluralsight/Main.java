d3cdac00096a75663bcfa96dddbff20dca46d23e
Marqae Morris <mamorris920@gmail.com>
2025-05-08 16:06:04 -0400

    setup for ccard game

---
+++ workbook-4/CardGame/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,25 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Deck deck = new Deck();
+        Hand hand1 = new Hand();
+        Hand hand2 = new Hand();
+
+        System.out.println("Enter the name for player 1");
+        String player1 = scanner.nextLine();
+        System.out.println("Enter the name for player 2");
+        String player2 = scanner.nextLine();
+
+
+        if (!player1 == true){
+
+        }
+    }
+
+}
