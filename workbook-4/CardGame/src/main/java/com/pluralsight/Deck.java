0243803e532004f102f7f5405d8dcd86f6b34a10
Marqae Morris <mamorris920@gmail.com>
2025-05-08 17:05:49 -0400

    added printing to see the hand and created if statements to handle cores to determine winner

--- workbook-4/CardGame/src/main/java/com/pluralsight/Deck.java
+++ workbook-4/CardGame/src/main/java/com/pluralsight/Deck.java
@@ -42,4 +42,4 @@ import java.util.Collections;
 
 
 
-}
+
--- workbook-4/CardGame/src/main/java/com/pluralsight/Main.java
+++ workbook-4/CardGame/src/main/java/com/pluralsight/Main.java
@@ -8,18 +8,39 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         Deck deck = new Deck();
-        Hand hand1 = new Hand();
-        Hand hand2 = new Hand();
+        deck.shuffle();
 
         System.out.println("Enter the name for player 1");
-        String player1 = scanner.nextLine();
+        String player1Name = scanner.nextLine();
         System.out.println("Enter the name for player 2");
-        String player2 = scanner.nextLine();
+        String player2Name = scanner.nextLine();
+
+        Hand player1 = new Hand();
+        player1.deal(deck.deal());
+        player1.deal(deck.deal());
+
+        Hand player2 = new Hand();
+        player2.deal(deck.deal());
+        player2.deal(deck.deal());
+
+        System.out.println(player1Name + "'s hand");
+        player1.print();
+
+        System.out.println(player2Name + "'s hand");
+        player2.print();
+
+        int player1Score = player1.getValue();
+        int player2Score = player2.getValue();
 
 
-        if (!player1 == true){
-
+        if(player1Score <= 21 &&(player1Score > player2Score || player2Score > 21)){
+            System.out.println(player1Name +  " wins!!");
+        }else if(player2Score <= 21 && (player2Score > player1Score || player1Score < 21)){
+            System.out.println(player2Name + " wins!!");
+        }else{
+            System.out.println("Tie!");
         }
+scanner.close();
     }
 
 }
