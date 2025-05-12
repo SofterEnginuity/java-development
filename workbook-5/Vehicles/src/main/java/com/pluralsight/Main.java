6dac7be7ed8b9ac897361a9e547c48b74996f41f
Marqae Morris <mamorris920@gmail.com>
2025-05-12 14:18:01 -0400

    modified methods in main

--- workbook-5/Vehicles/src/main/java/com/pluralsight/Main.java
+++ workbook-5/Vehicles/src/main/java/com/pluralsight/Main.java
@@ -3,22 +3,23 @@ package com.pluralsight;
 public class Main {
     public static void main(String[] args) {
 
-//        Vehicle vehicle = new Vehicle();
-//
-Car car = new Car();
-        Car.setMake("Honda");
-        Car.setModel("Civic");
-        Car.setYear(2024);
-        Car.setColor("Grey");
-        Car.setFuelCapacity(200);
-        Car.setNumOfPassengers(2);
-        Car.setPrice(42000);
-        Car.setSpeed(35);
-        Car.setCargoCapacity(1);
+        Vehicle vehicle = new Vehicle();
+        Car car = new Car();
 
+        car.setMake("Honda");
+        car.setModel("Civic");
+        car.setYear(2024);
+        car.setColor("Grey");
+        car.setFuelCapacity(200);
+        car.setNumOfPassengers(2);
+        car.setPrice(42000);
+        car.setSpeed(35);
+        car.setCargoCapacity(1);
 
-        Car.accelerate();
-        System.out.println(Car.getSpeed());
+        car.accelerate();
+        car.openTrunk();
+        System.out.println("Your current speed is " + car.getSpeed());
+
 
 
 
