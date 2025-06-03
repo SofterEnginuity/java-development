4d7e9b6697042a6b9df9456343497e8b13aa47ee
Marqae Morris <mamorris920@gmail.com>
2025-06-03 12:00:59 -0400

    commit catchup - was not being added previously

---
+++ workbook-7/basic-queries/Exercise 2 - Basic Queries.sql
@@ -0,0 +1,49 @@
+-- 1.) products
+
+-- 2.) 
+-- USE northwind;
+-- SELECT productID, productName, unitPrice
+-- FROM products;
+
+-- 3.)
+-- USE northwind;
+-- SELECT productID, productName, unitPrice
+-- FROM products
+-- ORDER BY unitPrice desc;
+
+-- 4.)
+-- USE northwind;
+-- SELECT productID, productName, unitPrice
+-- FROM products
+-- WHERE unitPrice < 7.5;
+
+-- 5.)
+-- USE northwind;
+-- SELECT productID, productName, unitPrice
+-- FROM products
+-- WHERE UnitsInStock < 100
+-- ORDER BY unitPrice;
+
+-- 6.)
+-- USE northwind;
+-- SELECT productID, productName, unitPrice, unitsInStock
+-- FROM products
+-- WHERE UnitsInStock > 100
+-- ORDER BY unitPrice, productName;
+
+
+-- 7.)
+-- USE northwind;
+-- SELECT productID, productName, unitPrice
+-- FROM products
+-- WHERE UnitsInStock < 1 AND UnitsOnOrder > 0;
+
+-- 8.)
+-- categories;
+
+-- 9.)
+-- USE northwind;
+-- SELECT *
+-- FROM categories;
+
+-- 10.)
