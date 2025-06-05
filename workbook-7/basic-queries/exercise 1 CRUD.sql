6eb873d35c1ec0ccd764f1fadbaf8c658e609a22
Marqae Morris <mamorris920@gmail.com>
2025-06-05 11:57:03 -0400

    crud practice SQL

---
+++ workbook-7/basic-queries/exercise 1 CRUD.sql
@@ -0,0 +1,28 @@
+
+-- 1.)
+-- INSERT INTO Suppliers(CompanyName)
+-- VALUES('Amazern');
+
+-- 2.)
+-- INSERT INTO Products(productName, supplierID)
+-- VALUES('AmazernProduct', 30);
+
+-- 3.)
+-- SELECT productName, supplierID
+-- FROM products
+-- ORDER BY supplierID;
+
+-- 4.)
+
+
+-- 6.)
+-- DELETE 
+-- FROM suppliers
+-- WHERE SupplierID = 30;
+
+-- 7.)
+-- DELETE 
+-- FROM products
+-- WHERE productID = 78;
+
+
--- workbook-7/basic-queries/exercise 4.sql
+++ workbook-7/basic-queries/exercise 4.sql
@@ -5,9 +5,16 @@
 -- WHERE unitPrice = (SELECT MAX(unitPrice) FROM products);
 
 -- 2.)
-USE northwind;
-SELECT orderID, ShipName, ShipAddress
-FROM shippers, orders
-WHERE ShipperID = 3;
+-- USE northwind;
+-- SELECT orderID, ShipName, ShipAddress
+-- FROM shippers, orders
+-- WHERE ShipperID = 3;
+-- /////Above is incorrect, because I manually looked for the ShipperID, Java cannot do that LOL
+-- USE northwind;
+-- SELECT orderID, ShipName, ShipAddress
+-- FROM shippers, orders
+-- WHERE ShipVia = (
+-- SELECT ShipperID 
+-- WHERE CompanyName = "Federal Shipping"
+-- );
 
--- 3.)
