7eae0226035bd82bcf92f288c773787a271c7999
Marqae Morris <mamorris920@gmail.com>
2025-06-09 14:31:41 -0400

    db connection for northwind

---
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        System.out.println("Hello, World!");
+
+Class.forName("com.mysql.cj.jdbc.Driver");
+
+Connection connection;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/northwind", "root", "P@ssw0rd");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+Statement statement = connection.createStatement();
+
+        String query = """
+                SELECT * FROM northwind.products""";
+        ResultSet results = statement.executeQuery(query);
+
+        while (results.next()) {
+            String product = results.getString("ProductName");
+            System.out.println(product);
+        }
+
+
+        connection.close();
+    }
+}
