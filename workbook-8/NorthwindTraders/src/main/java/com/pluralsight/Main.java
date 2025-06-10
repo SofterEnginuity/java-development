5f18836cc2d2e33a3991132db81be1a8a37646ae
Marqae Morris <mamorris920@gmail.com>
2025-06-10 08:49:32 -0400

    connection to db successful

--- workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
+++ workbook-8/NorthwindTraders/src/main/java/com/pluralsight/Main.java
@@ -3,7 +3,6 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        System.out.println("Hello, World!");
 
 Class.forName("com.mysql.cj.jdbc.Driver");
 
